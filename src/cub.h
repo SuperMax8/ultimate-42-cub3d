67a9988d5f6b3583d33bd00d97db3c297e02af72
Maximilien Rotceig <mrotceig@paul-f4Ar5s5.clusters.42paris.fr>
2025-04-11 15:43:21 +0200

    vec2f and cub structs

--- src/cub.h
+++ src/cub.h
@@ -6,11 +6,31 @@
 /*   By: mrotceig <mrotceig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 15:14:37 by mrotceig          #+#    #+#             */
-/*   Updated: 2025/04/11 15:19:19 by mrotceig         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:42:56 by mrotceig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_H
 # define CUB_H
+
+typedef struct vec2f
+{
+	float	x;
+	float	y;
+}			t_vec2f;
+
+typedef struct cub
+{
+	t_vec2f	ploc;
+	t_vec2f	pdir;
+	void	*mlx_ptr;
+	void	*window;
+	int		color_ceiling;
+	int		color_floor;
+	void	*img_n;
+	void	*img_s;
+	void	*img_e;
+	void	*img_w;
+}			t_cub;
 
 #endif
