4563b366e23dca1f01a5503b3abace7c9e65c513
SuperMax8 <mrotceig@42.fr>
2025-04-13 14:07:03 +0200

    simple collision system

--- src/movement.c
+++ src/movement.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movement.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 14:05:00 by max               #+#    #+#             */
+/*   Updated: 2025/04/13 14:05:20 by max              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub.h"
 
 #define MOVESPEED 0.02
@@ -6,14 +18,19 @@
 void moverot(t_cub *cub)
 {
 	t_vec2f vdir;
+	t_vec2f newloc;
 
 	if (cub->movement.x != 0 || cub->movement.y != 0)
 	{
 		cub->lastploc.x = cub->ploc.x;
 		cub->lastploc.y = cub->ploc.y;
 		vdir = yawtovec(cub->pyaw + vectoyaw(cub->movement));
-		cub->ploc.x += vdir.x * MOVESPEED;
-		cub->ploc.y += vdir.y * MOVESPEED;
+		vdir.x *= MOVESPEED;
+		vdir.y *= MOVESPEED;
+		newloc.x = cub->ploc.x + vdir.x;
+		newloc.y = cub->ploc.y + vdir.y;
+		if (!(newloc.x >= 0 && newloc.x < cub->mwidth && newloc.y >= 0 && newloc.y < cub->mheight && cub->map[(int) (cub->ploc.y + vdir.y * 15)][(int) (cub->ploc.x + vdir.x * 15)] == WALL))
+			cub->ploc = newloc;
 	}
 
 	cub->pyaw += cub->rotation * ROTSPEED;
